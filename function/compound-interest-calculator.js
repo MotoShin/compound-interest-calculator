8c1a96acfdfa68e0990d64af3e97ffddd141f5d3
Moto Shinriki <m8o8t2o8@gmail.com>
2021-03-19 19:48:19 +0900

    支払い計算を追加

--- function/compound-interest-calculator.js
+++ function/compound-interest-calculator.js
@@ -1,4 +1,4 @@
-const app = Vue.createApp({
+const appOne = Vue.createApp({
     data() {
         return {
             principal: 1000000,
@@ -28,4 +28,46 @@ const app = Vue.createApp({
     }
 })
 
-app.mount('#app')
+appOne.mount('#app1')
+
+const appTwo = Vue.createApp({
+    data() {
+        return {
+            principal: 10000000,
+            interestRate: 4,
+            term: 30,
+            display: 20,
+            items: []
+        }
+    },
+    methods: {
+        calculate() {
+            termMonth = this.term * 12
+            this.item = []
+            processAmount_one = this.principal
+            processAmount_two = this.principal
+            for (let i = 1; i <= this.display; i++) {
+                // 元利均等返済
+                interestPayment = processAmount_one * ((this.interestRate / 100) / 12)
+                interestPayment_one = Math.round(interestPayment)
+                paymentPerMonth = (processAmount_one * ((this.interestRate / 100) / 12) * Math.pow((1 + ((this.interestRate / 100) / 12)), termMonth)) / (Math.pow((1 + ((this.interestRate / 100) / 12)), termMonth) - 1)
+                paymentPerMonth_one = Math.round(paymentPerMonth)
+                principalPayment_one = paymentPerMonth_one - interestPayment_one
+
+                // 元金均等返済
+                principalPayment_two = Math.round(processAmount_two / termMonth)
+                interestPayment_two = Math.round(processAmount_two *  ((this.interestRate / 100) / 12))
+                paymentPerMonth_two = principalPayment_two + interestPayment_two
+
+                this.items.push([i + 'ヶ月目', processAmount_one, principalPayment_one, interestPayment_one, paymentPerMonth_one, processAmount_one - principalPayment_one, '', processAmount_two, principalPayment_two, interestPayment_two, paymentPerMonth_two, processAmount_two - principalPayment_two])
+                processAmount_one = processAmount_one - principalPayment_one
+                processAmount_two = processAmount_two - principalPayment_two
+            }
+        },
+        clear() {
+            this.items = []
+        }
+    }
+})
+
+appTwo.mount('#app2')
